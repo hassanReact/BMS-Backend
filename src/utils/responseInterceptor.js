8fda789f47b631a2ec6b964bfd50897001a7fe69
Muhammad Abdullah <abdullah03362@gmail.com>
2026-08-20 21:05:36 +0500

    re enable encrypted response

--- src/utils/responseInterceptor.js
+++ src/utils/responseInterceptor.js
@@ -47,70 +47,6 @@
 
 // ---------------------------------------------------
 
-// const responseInterceptor = (req, res, next) => {
-//     const oldSend = res.send;
-//     const oldJson = res.json;
-//     const oldCookie = res.cookie;
-
-//     console.log("oldSend---", oldSend);
-//     console.log("oldJson---", oldJson);
-//     console.log("oldCookie---", oldCookie);
-
-//     res.json = (data) => {
-//         console.log("data================<<<<<<<<<", data);
-
-//         if (data && data.status && data.status === 'error') {
-//             const formattedResponse = {
-//                 success: false,
-//                 data: {},
-//                 message: data.message || "Error occurred",
-//                 error: data.errorCode || data.message || "Unknown Error",
-//                 timestamp: new Date().toISOString(),
-//             };
-//             oldJson.call(res, formattedResponse);
-//         } else {
-//             const formattedResponse = {
-//                 success: true,
-//                 data: data || {},
-//                 message: data.message || "Success",
-//                 error: null,
-//                 timestamp: new Date().toISOString(),
-//             };
-//             oldJson.call(res, formattedResponse);
-//         }
-//     };
-
-//     res.send = (data) => {
-//         console.log("data================>>>>>>", data);
-
-//         if (data && typeof data === 'object') {
-//             res.json(data);
-//         } else {
-//             oldSend.call(res, data);
-//         }
-//     };
-
-//     res.cookie = (name, value, options) => {
-//         console.log(`Setting cookie ${name}`);
-//         oldCookie.call(res, name, value, options);
-//     };
-
-//     res.error = (error, statusCode = 500, message = "Internal Server Error") => {
-//         const formattedResponse = {
-//             success: false,
-//             data: {},
-//             message,
-//             error: error || message,
-//             timestamp: new Date().toISOString(),
-//         };
-//         res.status(statusCode).json(formattedResponse);
-//     };
-
-//     next();
-// };
-
-// export default responseInterceptor;
-
 import { encryptResponse } from "../core/common/crypto.js";
 
 const responseInterceptor = (req, res, next) => {
@@ -159,3 +95,5 @@ const responseInterceptor = (req, res, next) => {
 };
 
 export default responseInterceptor;
+
+
