ee9f95a7af6b319a1943931ba7a750ef36a92318
Muhammad Abdullah <abdullah03362@gmail.com>
2026-09-24 19:47:48 +0500

    migrate mongodb to postgresql

--- src/services/transactionalAccounts.services.js
+++ src/services/transactionalAccounts.services.js
@@ -1,25 +1,23 @@
-import  TransactionalAccounts  from "../models/transactionalAccounts.model.js";
+import AppDataSource from "../core/database/data-source.js";
 import { errorCodes, Message, statusCodes } from "../core/common/constant.js";
 import CustomError from "../utils/exception.js";
 
+const transactionalAccountRepository = AppDataSource.getRepository("TranstionalAccounts");
+
 
 export const createTransactionalAccount = async (req, res) => {
+  const { accountName, accountNumber, details, companyId } = req.body;
 
-    const {
-      accountName,
-      accountNumber,
-      details,
-      companyId
-    } = req.body;
+  const transactionalAccount = transactionalAccountRepository.create({
+    accountName,
+    accountNumber,
+    details,
+    companyId,
+  });
 
-    const transactionalAccount = await TransactionalAccounts.create({
-      accountName,
-      accountNumber,
-      details,
-      companyId
-    });
+  await transactionalAccountRepository.save(transactionalAccount);
 
-    return transactionalAccount;
+  return transactionalAccount;
 };
 
 export const getTransactionalAccountById = async (req, res) => {
@@ -33,9 +31,11 @@ export const getTransactionalAccountById = async (req, res) => {
     );
   }
 
-  const transactionalAccount = await TransactionalAccounts.findOne({
-    _id: id,
-    isDeleted: false
+  const transactionalAccount = await transactionalAccountRepository.findOne({
+    where: {
+      id,
+      isDeleted: false,
+    },
   });
 
   if (!transactionalAccount) {
@@ -52,72 +52,84 @@ export const getTransactionalAccountById = async (req, res) => {
 
 
 export const editTransactionalAccount = async (req, res) => {
-    const transactionalAccountId = req.query.id;
+  const transactionalAccountId = req.query.id;
 
-    if (!transactionalAccountId) {
-      return res.status(400).json({
-        message: "Transactional Account ID is required.",
-        errorCode: "transactionalAccount_id_missing",
-      });
-    }
+  if (!transactionalAccountId) {
+    return res.status(400).json({
+      message: "Transactional Account ID is required.",
+      errorCode: "transactionalAccount_id_missing",
+    });
+  }
 
-    const {
-      accountName,
-      accountNumber,
-      details,
-      companyId
-    } = req.body;
+  const { accountName, accountNumber, details, companyId } = req.body;
 
-    const updateData = {
-      accountName,
-      accountNumber,
-      details,
-      companyId
-    };
-    const updatedTransactionalAccount = await TransactionalAccounts.findByIdAndUpdate(
-      transactionalAccountId,
-      updateData,
-      { new: true, runValidators: true }
-    );
+  const existingTransactionalAccount = await transactionalAccountRepository.findOne({
+    where: {
+      id: transactionalAccountId,
+      isDeleted: false,
+    },
+  });
 
-    if (!updatedTransactionalAccount) {
-      return res.status(404).json({
-        message: "Transactional Account not found.",
-        errorCode: "Transactional Account Error",
-      });
-    }
+  if (!existingTransactionalAccount) {
+    return res.status(404).json({
+      message: "Transactional Account not found.",
+      errorCode: "Transactional Account Error",
+    });
+  }
 
-    return updatedTransactionalAccount;
-  
+  const updateData = {
+    accountName,
+    accountNumber,
+    details,
+    companyId,
+  };
+
+  Object.assign(existingTransactionalAccount, updateData);
+  const updatedTransactionalAccount = await transactionalAccountRepository.save(
+    existingTransactionalAccount
+  );
+
+  return updatedTransactionalAccount;
 };
 
 
 
 export const deleteTransactionalAccount = async (req, res) => {
-  const transactionalAccount = req.query.id;
+  const transactionalAccountId = req.query.id;
 
-  const transactionalAccountData = await TransactionalAccounts.findById(transactionalAccount);
+  const transactionalAccountData = await transactionalAccountRepository.findOne({
+    where: {
+      id: transactionalAccountId,
+      isDeleted: false,
+    },
+  });
+
   if (!transactionalAccountData) {
     throw new CustomError(
       statusCodes?.notFound,
-      Message?.notFound ,
+      Message?.notFound,
       errorCodes?.not_found
     );
-  } 
+  }
 
   transactionalAccountData.isDeleted = true;
-  await transactionalAccountData.save();
+  await transactionalAccountRepository.save(transactionalAccountData);
 
-  return transactionalAccountData
+  return transactionalAccountData;
 };
 
 export const getTransactionalAccount = async (req, res) => {
   const companyId = req.query.id;
 
-  const transactionalAccount = await TransactionalAccounts.find({
-    companyId,
-    isDeleted: false,
-  }).sort({ createdAt: -1 });
+  const transactionalAccount = await transactionalAccountRepository.find({
+    where: {
+      companyId,
+      isDeleted: false,
+    },
+    order: {
+      createdAt: "DESC",
+    },
+  });
 
   if (!transactionalAccount) {
     throw new CustomError(
@@ -126,5 +138,6 @@ export const getTransactionalAccount = async (req, res) => {
       errorCodes?.not_found
     );
   }
+
   return transactionalAccount;
 };
